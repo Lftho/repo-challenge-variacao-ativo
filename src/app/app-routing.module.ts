b17d7661e0f83baef7152c24f233f3bc055fda6b
Evolutionis <lftthomaz@outlook.com>
2023-11-10 11:00:54 -0300

    feat: roteamento da variação de ativos inicializando com lazy-loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/variation/PETR4.SA',
+    pathMatch: 'full'
+  },
+  {
+    path: 'variation/:symbol',
+    loadChildren: () => import('./core/variation/variation.module')
+      .then(m => m.VariationModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
